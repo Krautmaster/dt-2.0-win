b3ba8fc2f55126078f10585af0625db6850a50c7
Jan Ingwer Baer <jibaer@web.de>
2016-10-30 16:07:32 +0100

    windows: updates build instructions

--- win-helper/inst-pkg.sh
+++ win-helper/inst-pkg.sh
@@ -8,26 +8,22 @@ pacman -S --noconfirm mingw-w64-x86_64-gtk3
 pacman -S --noconfirm mingw-w64-x86_64-exiv2 mingw-w64-x86_64-lcms2 mingw-w64-x86_64-lensfun
 pacman -S --noconfirm mingw-w64-x86_64-openjpeg mingw-w64-x86_64-openjpeg2
 pacman -S --noconfirm mingw-w64-x86_64-sqlite3  
-pacman -S --noconfirm mingw-w64-x86_64-openexr  
-pacman -S --noconfirm mingw-w64-x86_64-libwebp
-pacman -S --noconfirm mingw-w64-x86_64-graphicsmagick
+pacman -S --noconfirm mingw-w64-x86_64-python2
+pacman -S --noconfirm mingw-w64-x86_64-openexr mingw-w64-x86_64-libwebp mingw-w64-x86_64-graphicsmagick
+pacman -S --noconfirm mingw-w64-x86_64-osm-gps-map mingw-w64-x86_64-pugixml mingw-w64-x86_64-libsoup
 pacman -S --noconfirm mingw-w64-x86_64-libxslt
 pacman -S --noconfirm mingw-w64-x86_64-curl
 pacman -S --noconfirm mingw-w64-x86_64-poppler
 pacman -S --noconfirm mingw-w64-x86_64-pkg-config
 pacman -S --noconfirm mingw-w64-x86_64-diffutils
-pacman -S --noconfirm mingw-w64-x86_64-python2
 pacman -S --noconfirm mingw-w64-x86_64-gnome-common
 pacman -S --noconfirm mingw-w64-x86_64-gobject-introspection
 pacman -S --noconfirm mingw-w64-x86_64-cmake
 pacman -S --noconfirm intltool
-pacman -S --noconfirm python2
 pacman -S --noconfirm tar
 pacman -S --noconfirm git
-pacman -S --noconfirm automake
-pacman -S --noconfirm autoconf
-pacman -S --noconfirm gtk-doc
-pacman -S --noconfirm m4
+# pacman -S --noconfirm automake autoconf m4
+# pacman -S --noconfirm gtk-doc
 pacman -S --noconfirm patch
 pacman -S --noconfirm zip unzip
 
